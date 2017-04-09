dccc9b012b7596e7cd0dd40a22a555c36b858827
Kristo Palo <kristopalo@hotmail.com>
2017-04-09 17:54:50 +0300

    Autoclicker increments + store table

--- app.js
+++ app.js
@@ -17,10 +17,44 @@ myApp.controller('cookieController', function ($scope, $interval) {
     $scope.cookies_autoclicked = 0;
     $scope.cookies_baked = 0;
 
-    var grandma_interval = 5000;
+    var grandma_interval = 3000;
     var autoclicker_interval = 1000;
 
-    $scope.incrementCookieCount = function (increment_size) {
+    $scope.cursor_status = "BUY"
+    $scope.grandma_status = "BUY"
+    $scope.autoclicker_status = "BUY"
+
+    $scope.cursor_bought = false;
+    $scope.grandma_bought = false;
+    $scope.autoclicker_bought = false;
+
+    $scope.incrementCookieCount = function (e, increment_size) {
+        var obj = $("#clone").clone();
+        $("body").append(obj);
+
+        obj.html("+" + increment_size);
+
+        if (e !== null) {
+            obj.offset({ left: e.pageX - 10, top: e.pageY - 25 });
+        } else {
+            var position = $("#cookie").position();
+            var img = document.getElementById('cookie');
+            var width = img.clientWidth;
+            var height = img.clientHeight;
+            obj.offset({
+                left: position.left + width / 2 + 20,
+                top: position.top + height / 2 + 20
+            });
+        }
+        obj.css("color: white");
+        obj.animate({
+            "top": "-=100px",
+            "opacity": 0.10,
+            "left": "+=20px"
+        }, 1000, "linear", function () {
+            $(this).remove();
+        });
+
         $scope.cookie_count += increment_size;
     };
 
@@ -35,6 +69,10 @@ myApp.controller('cookieController', function ($scope, $interval) {
 
     $scope.buyNewCursor = function (next_val) {
         if (canBuy(next_val)) {
+            if (!$scope.cursor_bought) {
+                $scope.cursor_status = "UPGRADE";
+                $scope.cursor_bought = true;
+            }
             $scope.next_cursor_cost = next_val + 2 * $scope.cursor_level;
             $scope.cursor_level += 1;
             $scope.click_value += $scope.cursor_level;
@@ -43,12 +81,16 @@ myApp.controller('cookieController', function ($scope, $interval) {
 
     $scope.buyNewGrandma = function (next_val) {
         if (canBuy(next_val)) {
+            if (!$scope.grandma_bought) {
+                $scope.grandma_status = "UPGRADE";
+                $scope.grandma_bought = true;
+            }
             $scope.next_grandma_cost = next_val + 2 * $scope.grandma_level;
             $scope.grandma_level += 1;
             grandma_interval -= 100;
             $interval(function () {
-                $scope.incrementCookieCount($scope.grandma_level);
-                $scope.cookies_baked += $scope.grandma_level; 
+                $scope.incrementCookieCount(null, $scope.grandma_level);
+                $scope.cookies_baked += $scope.grandma_level;
                 addToTotal($scope.grandma_level);
             }, grandma_interval);
         }
@@ -57,11 +99,15 @@ myApp.controller('cookieController', function ($scope, $interval) {
 
     $scope.buyNewAutoclicker = function (next_val) {
         if (canBuy(next_val)) {
+            if (!$scope.autoclicker_bought) {
+                $scope.autoclicker_status = "UPGRADE";
+                $scope.autoclicker_bought = true;
+            }
             $scope.next_autoclicker_cost = next_val + 2 * $scope.autoclicker_level;
             $scope.autoclicker_level += 1;
             autoclicker_interval -= 50;
             $interval(function () {
-                $scope.incrementCookieCount($scope.autoclicker_level);
+                $scope.incrementCookieCount(null, $scope.autoclicker_level);
                 $scope.cookies_autoclicked += $scope.autoclicker_level;
                 addToTotal($scope.autoclicker_level);
             }, autoclicker_interval);
@@ -71,18 +117,5 @@ myApp.controller('cookieController', function ($scope, $interval) {
     function addToTotal(val) {
         $scope.cookies_clicked += val;
     }
-    
-$("#cookie").click(function(e) {
-    
-    var obj = $("#clone").clone();
-    $("body").append(obj);
-    obj.html("+"+ $scope.click_value);
-	obj.offset({left: e.pageX-10, top: e.pageY-25});
-    
-    obj.animate({"top": "-=100px"}, 1000, "linear", function() {
-		$(this).remove();
-	});	
-
-});
 
 });
